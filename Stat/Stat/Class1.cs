e22ae4e71f6439efce32ee71475833f17918dc53
Ludovic Merel <Ludo.m@live.fr>
2017-12-14 13:32:50 +0100

    merge master & refactoringunittest

---
+++ Stat/Stat/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperComputer;
+
+namespace Stat
+{
+    public class Stat : Function<string>
+    {
+        private string helpMessage = "Cette fonction permet d'additionner/soustraire deux valeurs entre elles.\r\n" +
+                                     "Les valeurs peuvent être réelles ou non. Le type de la variable de retour est " +
+                                     "double";
+
+        private string[] parametersName = { "first", "second" };
+
+        //Renvoie le nom de la fonction
+        public string Name { get { return "Adder"; } }
+
+        //Renvoir un message d'aide disant ce que fais la fonction et le type des paramétres
+        public string HelpMessage { get { return helpMessage; } }
+
+        //Renvoie un liste avec les noms des paramétres
+        public string[] ParametersName
+        {
+            get { return parametersName; }
+        }
+
+        //Calcul la somme des deux paramétres
+        public string Evaluate(string[] args)
+        {
+
+            if (args.Length == 2)
+            {
+                try
+                {
+                    return "";
+                }
+                //léve une exception si une erreur est produite lors du calcul
+                catch { throw new EvaluationException("Un probléme est survenu lors du calcul"); }
+            }
+            //léve une exception si une erreur est produite si il n'y a pas deux valeurs
+            else { throw new EvaluationException("Veuillez rentrez deux valeurs"); }
+        }
+    }
+}
